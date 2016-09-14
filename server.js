e3f85f406c3c28ea1141568d609a540e8cff624a
broezer@gmail.com <broezer@gmail.com>
2016-09-14 21:04:01 +0200

    add routes

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var host = '127.0.0.1';
 var port = '8080';
 
+var routes = require('./app/index.js');
+
 var app = express();
+
+app.use(routes);
 
 app.listen(port, host);
 console.log('Server started on nodetogetherAMS');
