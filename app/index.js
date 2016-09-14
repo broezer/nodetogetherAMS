9ae6b9e4446043ab512c732a5a30766309080258
broezer@gmail.com <broezer@gmail.com>
2016-09-14 21:56:24 +0200

    add motivations with pickone in route parameters

--- app/index.js
+++ app/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
 
+var motivations = require('motivations');
+var pickOne = require('pick-one');
+
+
 app.get('/', function(request, response){
-  response.send('hello world')
+  response.send(pickOne(motivations))
+});
+
+app.get('/all', function(request, response){
+  var id = request.params.motivationID
+  response.send(motivations);
+});
+
+app.get('/:motivationID', function(request, response){
+  var id = request.params.motivationID
+  response.send(motivations[id]);
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var host = process.env.HOST || '0.0.0.0';
 var port = process.env.PORT || '8080';
 
-var routes = require('./app/index.js');
+var routes = require('./app');
 
 var app = express();
 
